b0bda1f3ed185ab94751fb1906f4de141d24ed2d
asymp <asymptotelian@gmail.com>
2024-04-12 10:29:07 +0300

    fixed markup and reformatted code

--- Chap10/mandelbrot2.js
+++ Chap10/mandelbrot2.js
@@ -18,67 +18,68 @@ var cy = -1.0;
 var max = 100;             /* number of interations per point */
 
 var n = 1024;
-var m =1024;
+var m = 1024;
 
 var program;
 
 //----------------------------------------------------------------------------
 
 onload = function init() {
-    canvas = document.getElementById( "gl-canvas" );
+    canvas = document.getElementById("gl-canvas");
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.viewport(0, 0, canvas.width, canvas.height);
 
 
     // Create and initialize a buffer object
 
     var points = [
-
-    vec4(0.0, 0.0, 0.0, 1.0),
-	vec4(0.0, 1.0, 0.0, 1.0),
-	vec4(1.0, 1.0, 0.0, 1.0),
-    vec4(1.0, 1.0, 0.0, 1.0),
-	vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(0.0, 0.0, 0.0, 1.0)
-];
+        vec4(0.0, 0.0, 0.0, 1.0),
+        vec4(0.0, 1.0, 0.0, 1.0),
+        vec4(1.0, 1.0, 0.0, 1.0),
+        vec4(1.0, 1.0, 0.0, 1.0),
+        vec4(1.0, 0.0, 0.0, 1.0),
+        vec4(0.0, 0.0, 0.0, 1.0)
+    ];
 
     // Load shaders and use the resulting shader program
 
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     // set up vertex arrays
     var buffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    var vPosition = gl.getAttribLocation(program, "vPosition");
 
-    gl.enableVertexAttribArray( vPosition );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0,0);
-    gl.bufferData( gl.ARRAY_BUFFER,  flatten(points), gl.STATIC_DRAW );
+    gl.enableVertexAttribArray(vPosition);
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    gl.uniform1f( gl.getUniformLocation(program, "scale"), scale);
-    gl.uniform1f( gl.getUniformLocation(program, "cx"), cx);
-    gl.uniform1f( gl.getUniformLocation(program, "cy"), cy);
+    gl.uniform1f(gl.getUniformLocation(program, "scale"), scale);
+    gl.uniform1f(gl.getUniformLocation(program, "cx"), cx);
+    gl.uniform1f(gl.getUniformLocation(program, "cy"), cy);
 
 
-    document.getElementById("Center X").onchange = function(event) {
+    document.getElementById("Center X").onchange = function (event) {
         cx = event.target.value;
-        gl.uniform1f( gl.getUniformLocation(program, "cx"), cx);
+        gl.uniform1f(gl.getUniformLocation(program, "cx"), cx);
     };
-    document.getElementById("Center Y").onchange = function(event) {
+    document.getElementById("Center Y").onchange = function (event) {
         cy = event.target.value;
-        gl.uniform1f( gl.getUniformLocation(program, "cy"), cy);
+        gl.uniform1f(gl.getUniformLocation(program, "cy"), cy);
     };
-    document.getElementById("Size").onchange = function(event) {
-       scale = 1.0/event.target.value;
-       gl.uniform1f( gl.getUniformLocation(program, "scale"), scale);
+    document.getElementById("Size").onchange = function (event) {
+        scale = 1.0 / event.target.value;
+        gl.uniform1f(gl.getUniformLocation(program, "scale"), scale);
     };
 
 
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     render();
@@ -87,8 +88,8 @@ onload = function init() {
 //----------------------------------------------------------------------------
 
 
-var render = function() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 6 );
+var render = function () {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
     requestAnimFrame(render);
 }
